8845acce6aca29375ec70519f92ed80ae9b76f96
PutraMegantara <92736847+PutraMegantara@users.noreply.github.com>
2021-11-14 21:09:58 +0700

    Delete if 2.py

--- if 2.py
+++
@@ -1,7 +0,0 @@
-print ("/tprogram mengurutkan Bilangan/t")
-bilangan = []
-for i in range(1,6):
-    bilangan.append(int(input("Masukan Bilangan : ")))
-bilangan.sort()
-print("Urutan Bilangan",bilangan)
-    
