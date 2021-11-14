65b2554f0e6af86cf199b438b3eeb953ad0ab7af
PutraMegantara <92736847+PutraMegantara@users.noreply.github.com>
2021-11-14 21:10:14 +0700

    Delete if 4.py

--- if 4.py
+++
@@ -1,9 +0,0 @@
-from random import random
-
-a = int(input("Masukan Bilangan :"))
-while a==a:
-    break
-for i in range(a):
-    bil = random()%0.5
-    print ("perulangan ke- :", bil)
-print ("selesai")
