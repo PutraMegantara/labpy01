9069a342bc5d80f891dc788b7981d1e77105adbc
Megantara <mmegantara10@gmail.com>
2022-01-08 16:25:03 +0700

    Menambahkan python

---
+++ python/if 4.py
@@ -0,0 +1,9 @@
+from random import random
+
+a = int(input("Masukan Bilangan :"))
+while a==a:
+    break
+for i in range(a):
+    bil = random()%0.5
+    print ("perulangan ke- :", bil)
+print ("selesai")
